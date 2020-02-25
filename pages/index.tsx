eba03d15faf4665d46936dffc51bd3b013db4267
Jongkeun <dldkfod@naver.com>
2020-02-25 21:54:02 +0900

    added RefreshTime sample

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import { NextPage } from "next";
-import styled from "styled-components";
 import Layout from "../components/Layout";
 import SearchBar from "../components/SearchBar";
 import ForecastMap from "../components/ForecastMap";
 import { Fragment } from "react";
+import RefreshTime from "../components/RefreshTime";
 
 type Props = {
   isServer: boolean;
@@ -14,6 +14,7 @@ const Index: NextPage<Props> = ({ isServer, pathname }) => (
   <Layout pathname={pathname}>
     <Fragment>
       <SearchBar />
+      <RefreshTime />
       <ForecastMap />
     </Fragment>
   </Layout>
